f9c345c5722cb583d0f8b6cf044f7ef9e349d5f6
aorganx <alexorgan@wp.pl>
2023-07-20 12:10:51 +0100

    C07

---
+++ src/R04/C07/Main.java
@@ -0,0 +1,12 @@
+package R04.C07;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++){
+            System.out.print(i + " ");
+            if(i == 99){
+                break;
+            }
+        }
+    }
+}
