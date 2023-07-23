575008fa4810b9f4a61b12af519d84f57b7020df
ThatPalAl <alekontox@gmail.com>
2023-07-23 17:14:03 +0100

    Instance Initializer Block
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/R05/E11/Main.java
@@ -0,0 +1,12 @@
+package R05.E11;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inside main:");
+        new Mugs();
+        System.out.println("new Mugs()- finished");
+        new Mugs(2);
+        Mugs mugs1 = new Mugs();
+        mugs1.mug2.f(5);
+    }
+}
