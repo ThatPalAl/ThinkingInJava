76f0ef35b4f41295a3e4c955b3217669859e6b88
ThatPalAl <alekontox@gmail.com>
2023-08-23 19:13:55 +0200

    Wind child class
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r08/examples/e1/Brass.java
@@ -0,0 +1,7 @@
+package r08.examples.e1;
+
+public class Brass extends Instrument {
+    public void play(Note n){
+        System.out.println("Brass.play()" + n);
+    }
+}
---
+++ src/r08/examples/e1/Stringed.java
@@ -0,0 +1,8 @@
+package r08.examples.e1;
+
+public class Stringed extends Instrument {
+    public void play(Note n){
+        System.out.println("Stringed.play()" + n);
+    }
+
+}
