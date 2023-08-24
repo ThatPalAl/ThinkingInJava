e6977ecd56851515206ce7d1aad4d641d79d5398
ThatPalAl <alekontox@gmail.com>
2023-08-24 22:21:31 +0200

    overriding static methods - casting upwards
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r08/examples/e06/StaticSub.java
@@ -0,0 +1,12 @@
+package r08.examples.e06;
+
+public class StaticSub extends StaticSuper {
+    public static String staticGet(){
+        return "derivative version of staticGet()";
+    }
+
+    public String dynamicGet(){
+        return "derivative version of dynamicGet()";
+    }
+
+}
---
+++ src/r08/examples/e06/StaticSuper.java
@@ -0,0 +1,10 @@
+package r08.examples.e06;
+
+public class StaticSuper {
+    public static String staticGet(){
+        return "staticGet base version";
+    }
+    public String dynamicGet(){
+        return "dynamicGet base version";
+    }
+}
