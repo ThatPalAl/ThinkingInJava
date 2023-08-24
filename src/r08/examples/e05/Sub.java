6be7e3b0c03d73bb8c111b64d428ce18932b5459
ThatPalAl <alekontox@gmail.com>
2023-08-24 21:44:37 +0200

    static fields and methods - casting up
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r08/examples/e05/Sub.java
@@ -0,0 +1,13 @@
+package r08.examples.e05;
+
+public class Sub extends Super {
+    public int field = 1;
+
+    public int getField() {
+        return field;
+    }
+
+    public int getSuperField(){
+        return super.field;
+    }
+}
