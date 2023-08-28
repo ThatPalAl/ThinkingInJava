ba3770ce6d391e15d0960aac1ab8c065dc116bd2
ThatPalAl <alekontox@gmail.com>
2023-08-28 20:07:29 +0200

    Casting up and down while compiling
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r08/examples/e13/MoreUseful.java
@@ -0,0 +1,9 @@
+package r08.examples.e13;
+
+public class MoreUseful extends Useful {
+    public void f(){}
+    public void g(){}
+    public void u(){}
+    public void v(){}
+    public void w(){}
+}
---
+++ src/r08/examples/e13/Useful.java
@@ -0,0 +1,6 @@
+package r08.examples.e13;
+
+public class Useful {
+    public void f(){}
+    public void g(){}
+}
