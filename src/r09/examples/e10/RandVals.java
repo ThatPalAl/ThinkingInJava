352d7a37bdb5600675978e058bb5f06144411fff
ThatPalAl <alekontox@gmail.com>
2023-10-15 14:41:16 +0200

    static fields in interfaces- initialized while loading class
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r09/examples/e10/RandVals.java
@@ -0,0 +1,11 @@
+package r09.examples.e10;
+
+import java.util.Random;
+
+public interface RandVals {
+    Random RAND = new Random(47);
+    int RANDOM_INT = RAND.nextInt(10);
+    long RANDOM_LONG = RAND.nextLong() * 10;
+    float RANDOM_FLOAT = RAND.nextFloat() * 10;
+    double RANDOM_DOUBLE = RAND.nextDouble() * 10;
+}
