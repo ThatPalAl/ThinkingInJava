d576b7b47d9ddb94e00e4323f0b327b15bee8443
ThatPalAl <alekontox@gmail.com>
2023-11-07 21:33:29 +0100

    Intro to collections
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.console();
-        System.getProperties().list(System.out);
-        System.out.println(System.getProperty("user.name"));
-        System.out.println(System.getProperty("java.library.path"));
-        
+
     }
 }
