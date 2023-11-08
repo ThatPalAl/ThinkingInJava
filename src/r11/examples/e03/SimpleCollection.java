136c338175d6cc806b331b621d2cb6cb48ba9a69
ThatPalAl <alekontox@gmail.com>
2023-11-08 20:23:20 +0100

    Collections methods and behavior
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r11/examples/e03/SimpleCollection.java
@@ -0,0 +1,16 @@
+package r11.examples.e03;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SimpleCollection {
+    public static void main(String[] args) {
+        Collection<Integer> c = new ArrayList<Integer>();
+        for(int i = 0; i < 10; i++){
+            c.add(i);
+        }
+        for(Integer i : c){
+            System.out.print(i + ". ");
+        }
+    }
+}
