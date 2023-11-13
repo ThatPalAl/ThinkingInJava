7b2fa1cc9d07a6610184b37508fd99aa58853747
ThatPalAl <alekontox@gmail.com>
2023-11-13 20:40:10 +0100

    ArrayList operations
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r11/examples/e06/Pet.java
@@ -0,0 +1,15 @@
+package r11.examples.e06;
+
+public class Pet {
+
+    private String name;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
