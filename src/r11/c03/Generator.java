95346ee86eed6c3e67d36667cbae8c28e8718f4a
ThatPalAl <alekontox@gmail.com>
2023-11-15 21:04:02 +0100

    Array list with custom object type
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r11/c03/Generator.java
@@ -0,0 +1,5 @@
+package r11.c03;
+
+public class Generator {
+
+}
---
+++ src/r11/c06/Fruit.java
@@ -0,0 +1,14 @@
+package r11.c06;
+
+public class Fruit {
+
+    private String name;
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
