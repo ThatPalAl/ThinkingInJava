21143e4daedbf99ffcd833e40bdc706bdb854c64
ThatPalAl <alekontox@gmail.com>
2023-11-21 19:04:55 +0100

    Person constructor changed
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

--- src/r07/c05/Person.java
+++ src/r07/c05/Person.java
@@ -2,14 +2,24 @@ package r07.c05;
 
 public class Person {
     int age;
+    String name;
     public Person(int age) {
         this.age = age;
         System.out.println("Person's age : " + age);
 
     }
 
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Person(String name){
+        this.name = name;
+    }
+
     @Override
     public String toString() {
-        return this.toString() + " ";
+
+        return name;
     }
 }
