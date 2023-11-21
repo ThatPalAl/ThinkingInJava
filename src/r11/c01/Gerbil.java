7ce5e047c13e4a4a31d4d1f92133b77d8cc78e1f
ThatPalAl <alekontox@gmail.com>
2023-11-21 20:26:35 +0100

    Constructor change for next tasks purposes
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

--- src/r11/c01/Gerbil.java
+++ src/r11/c01/Gerbil.java
@@ -10,4 +10,9 @@ public class Gerbil {
     public void hop(){
         System.out.println("Gerbil nr: " + gerbilNr);
     }
+
+    @Override
+    public String toString() {
+        return "Gerbil nr : " + gerbilNr;
+    }
 }
