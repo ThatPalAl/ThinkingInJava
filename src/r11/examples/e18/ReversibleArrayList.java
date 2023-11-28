96a8518dedb29eaf4e0af4a99812d466bd21ea7a
ThatPalAl <alekontox@gmail.com>
2023-11-28 19:29:25 +0100

    Adapter method
    Signed-off-by: ThatPalAl <alekontox@gmail.com>

---
+++ src/r11/examples/e18/ReversibleArrayList.java
@@ -0,0 +1,30 @@
+package r11.examples.e18;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ReversibleArrayList<T> extends ArrayList<T> {
+    public ReversibleArrayList(Collection<T> c){super(c);}
+    public Iterable<T> reversed(){
+        return new Iterable<T>() {
+            @Override
+            public Iterator<T> iterator() {
+                return new Iterator<T>() {
+                    int current = size() - 1;
+
+                    public boolean hasNext() {
+                        return current > -1;
+                    }
+
+                    public T next() {
+                        return get(current--);
+                    }
+                    public void remove(){
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
+    }
+}
